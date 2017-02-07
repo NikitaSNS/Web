41c6eb71d08ca4170057e1d43338bdc389d9b2e9
Nikita Savanchuk <savanchuk.nikita@gmail.com>
2017-02-07 21:31:18 +0300

    init 3 pract

--- 5/js/custom.js
+++ 5/js/custom.js
@@ -8,5 +8,4 @@ function replaceString(string) {
     return string.replace(/(^(?:[^a-zA-Z0-9]+))|(?:[^a-zA-Z0-9]+$)/g, "");
 }
 
-
 console.log(replaceString('!!!@@@||!43W!!w!Aasdfdas1234!!#!@EDAS@Ddsa@!!as@!!!@@@||!!'));
