61a100c7bc9718357461c8264c789252e87bce0c
Nikita Savanchuk <savanchuk.nikita@gmail.com>
2017-02-25 19:29:23 +0300

    8 added

---
+++ 8/js/main.js
@@ -0,0 +1,36 @@
+document.querySelectorAll('nav > ul > li').forEach(function(e) {
+
+    let eventClass = 'active';
+
+    e.addEventListener('mouseover', function() {
+        this.className = eventClass;
+        console.log(this.parentNode.children);
+
+        let childs = this.parentNode.children;
+
+        for (let i = 0; i < childs.length; i++) {
+            if (childs[i].className === eventClass) {
+                childs[i].firstChild.style.color = 'rgba(255, 216, 0, 1)';
+                continue;
+            }
+            childs[i].firstChild.style.color = childs[i].className === eventClass ? 'rgba(255, 216, 0, 1)' : 'rgba(255, 216, 0, 0.5)'
+        }
+    });
+
+    e.addEventListener('mouseleave', function() {
+        this.className = '';
+
+        let childs = this.parentNode.children;
+
+        for (let i = 0; i < childs.length; i++) {
+            if (childs.className === eventClass) {
+                return;
+            }
+        }
+
+        for (let i = 0; i < childs.length; i++) {
+            childs[i].firstChild.style.color = '#ffc000';
+        }
+    });
+
+});
