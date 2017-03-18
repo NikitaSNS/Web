6cc90343fecbda9f25e9c8ab33d685e2e6c9c3a8
NikitaSNS <savanchuk.nikita@gmail.com>
2017-03-18 22:34:22 +0300

    7 pract

--- 7/js/main.js
+++ 7/js/main.js
@@ -1,11 +1,11 @@
 let img = document.getElementById('top_img');
-img.onclick = function() {
+img.onclick = function () {
     setNextText('description');
 };
 
-img.onmouseover = function() {
+img.onmouseover = function () {
     setNextText('caption');
-}
+};
 
 function setNextText(id) {
     let element = document.getElementById(id);
@@ -17,7 +17,13 @@ function getNextText() {
 }
 
 function getText(index) {
-    return ["a", "b", "c", "d", "e"][index];
+    return [
+        "Learn Java, they said it'll be fun, they said",
+        "Lorem",
+        "Del",
+        "PHP TRUE",
+        "How to learn C++ in 21 days"
+    ][index];
 }
 
 function getRandomInt(min, max) {
