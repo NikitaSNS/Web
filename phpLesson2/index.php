d2d4b62f92e69d3d749736d4ebb281427a2e7d71
NikitaSNS <savanchuk.nikita@gmail.com>
2017-03-20 12:42:22 +0300

    less2

--- phpLesson2/index.php
+++ phpLesson2/index.php
@@ -1,6 +1,12 @@
 <?php
 
+header('Content-type: text/html; charset=utf-8');
+
 $connect = new mysqli('localhost', 'root', '', 'gruber');
+
+$connect->set_charset('utf8');
+
+//$data = iconv('utf-8', 'windows-1251', $data);
 
 $id = 2001;
 echo '<pre>';
