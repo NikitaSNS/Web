958e5f7c7b219823035f1b2fcbb2b1b592f92ca3
NikitaSNS <savanchuk.nikita@gmail.com>
2017-03-22 13:53:36 +0300

    cache hack

---
+++ phpLesson5/index.php
@@ -0,0 +1,18 @@
+<?php
+
+function noCache()
+{
+    $headers = [
+        'Expires: Wed, 22 Mar 2016 13:36:59 GMT', // . date('Y,m,d H:m')
+        'Last-Modify: ' . gmdate('D, d M Y H:i:s' . ' GMT'),
+        'Cache-Control: no-cache, must-revalidate',
+        'Cache-Control: post-check=0, pre-check=0',
+        'Cache-Control: max-age=0',
+        'Pragma: no-cache',
+    ];
+    foreach ($headers as $header) {
+        header($header);
+    }
+}
+
+noCache();
