2c0641746d33878e5ffe4c54c5eb464be7627b8d
NikitaSNS <savanchuk.nikita@gmail.com>
2017-03-26 18:57:12 +0300

    16

---
+++ 16/src/controllers/AccountController.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: Nikita
+ * Date: 26.03.2017
+ * Time: 18:29
+ */
+class AccountController
+{
+    public function indexAction()
+    {
+        echo 'index';
+        file_get_contents();
+
+        return true;
+    }
+
+    public function newsAction()
+    {
+        echo 'news';
+
+        return true;
+    }
+}
