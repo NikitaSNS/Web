c91b521d36d413b4fd9573207312325a0291cd0e
NikitaSNS <savanchuk.nikita@gmail.com>
2017-04-09 13:13:59 +0300

    16

--- 16/profile.php
+++ 16/profile.php
@@ -16,20 +16,19 @@ if (isset($_POST['submit'])) {
         new FormField('first_name', '[A-Za-zА-Яа-яЁё]{2,10}', 'Введите правильное имя'),
         new FormField('last_name', '[A-Za-zА-Яа-яЁё]{2,14}', 'Введите правильную фамилию'),
         new FormField('gender', '\w+', 'Введите правильный пол'),
-        new FormField('age', '\w+', 'Введите правильный возраст'),
+        new FormField('age', '0?[1-9]|[1-9][0-9]', 'Введите правильный возраст'),
         new FormField('date_of_birth', '(1899|19[0-9]{2}|200[0-9])-(0[1-9]|1[012])-(0[1-9]|1[0-9]|2[0-9]|3[01])',
             'Введите правельную дату рождения'),
-        new FormField('phone', '(0?[1-9]|[1-9][0-9])', 'Введите правильный номер телефона'),
+        new FormField('phone', '((8|\+7)[\- ]?)?(\(?\d{3}\)?[\- ]?)?[\d\- ]{7,10}', 'Введите правильный номер телефона'),
         new FormField('login', '\w{3,10}', 'Введите правильный логин'),
         new FormField('password_sms', '\w{5}', 'Введите правильный пароль смс'),
         new FormField('password', '(^(?!.*[А-Яа-яЁё])(?=(?:.*[A-Z]){1})(?=(?:.*[^A-Za-z0-9]){2}).{5,13})',
             'Введите правильный пароль'),
-        new FormField('submit', '\w', 'Нажмите кнопку :)'),
     ];
     foreach ($requiredFields as $field) {
         if (!isset($_POST[$field->getFieldName()]) ||
             empty($_POST[$field->getFieldName()]) ||
-            !preg_match('/^(' . $field->getPattern() . ')+$/', $_POST[$field->getFieldName()])
+            !preg_match('/^(' . $field->getPattern() . ')$/', $_POST[$field->getFieldName()])
         ) {
             $error = $field->getError();
             break;
